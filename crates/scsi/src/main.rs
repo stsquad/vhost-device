b37bda1aa25b3ab915f670f3ff7e31b0cea5a28f
Gaelan Steele <gbs@canishe.com>
2023-03-08 16:08:15 +0100

    scsi: Add virtio daemon
    This adds the virtio-specific parts that use the previously formed
    interfaces and scsi emulation in order to build a daemon that offers files
    from the host system as drives to the guest.
    
    The vast majority of this work was done by Gaelan Steele as part of a
    GSoC project [1][2].
    
    [1] https://github.com/rust-vmm/vhost-device/pull/4
    [2] https://gist.github.com/Gaelan/febec4e4606e1320026a0924c3bf74d0
    
    Co-developed-by: Erik Schilling <erik.schilling@linaro.org>
    Signed-off-by: Erik Schilling <erik.schilling@linaro.org>
    Signed-off-by: Gaelan Steele <gbs@canishe.com>

--- crates/scsi/src/main.rs
+++ crates/scsi/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello world");
+// SPDX-License-Identifier: Apache-2.0 or BSD-3-Clause
+
+mod backend;
+mod scsi;
+mod vhu_scsi;
+mod virtio;
+
+fn main() -> backend::Result<()> {
+    backend::scsi_init()
 }
